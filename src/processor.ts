e2dafcd01d99cb43685b405c4f13685ea0d698a4
tubleronchik <m.bystramovich@gmail.com>
2022-03-24 18:32:41 +0300

    add types for robonomics. Start fetching Balance Transfers events

--- src/processor.ts
+++ src/processor.ts
@@ -7,51 +7,54 @@ import {
 import { lookupArchive } from "@subsquid/archive-registry";
 import { Account, HistoricalBalance } from "./model";
 import { BalancesTransferEvent } from "./types/events";
+import { SimpleConsoleLogger } from "typeorm";
 
-const processor = new SubstrateProcessor("kusama_balances");
+const processor = new SubstrateProcessor("robonomics_balances");
 
-processor.setTypesBundle("kusama");
+processor.setTypesBundle("robonomicsTypesBundle.json");
 processor.setBatchSize(500);
 
 processor.setDataSource({
-  archive: lookupArchive("kusama")[0].url,
-  chain: "wss://kusama-rpc.polkadot.io",
+  archive: 'https://robonomics.indexer.gc.subsquid.io/v4/graphql',
+  chain: "wss://kusama.rpc.robonomics.network",
 });
 
 processor.addEventHandler("balances.Transfer", async (ctx) => {
   const transfer = getTransferEvent(ctx);
-  const tip = ctx.extrinsic?.tip || 0n;
-  const from = ss58.codec("kusama").encode(transfer.from);
-  const to = ss58.codec("kusama").encode(transfer.to);
+  console.log(`transfer ${transfer}`)
+  console.log(ctx)
+  // const tip = ctx.extrinsic?.tip || 0n;
+  // const from = ss58.codec(32).encode(transfer.from);
+  // const to = ss58.codec(32).encode(transfer.to);
 
-  const fromAcc = await getOrCreate(ctx.store, Account, from);
-  fromAcc.balance = fromAcc.balance || 0n;
-  fromAcc.balance -= transfer.amount;
-  fromAcc.balance -= tip;
-  await ctx.store.save(fromAcc);
+  // const fromAcc = await getOrCreate(ctx.store, Account, from);
+  // fromAcc.balance = fromAcc.balance || 0n;
+  // fromAcc.balance -= transfer.amount;
+  // fromAcc.balance -= tip;
+  // await ctx.store.save(fromAcc);
 
-  const toAcc = await getOrCreate(ctx.store, Account, to);
-  toAcc.balance = toAcc.balance || 0n;
-  toAcc.balance += transfer.amount;
-  await ctx.store.save(toAcc);
+  // const toAcc = await getOrCreate(ctx.store, Account, to);
+  // toAcc.balance = toAcc.balance || 0n;
+  // toAcc.balance += transfer.amount;
+  // await ctx.store.save(toAcc);
 
-  await ctx.store.save(
-    new HistoricalBalance({
-      id: `${ctx.event.id}-to`,
-      account: fromAcc,
-      balance: fromAcc.balance,
-      date: new Date(ctx.block.timestamp),
-    })
-  );
+  // await ctx.store.save(
+  //   new HistoricalBalance({
+  //     id: `${ctx.event.id}-to`,
+  //     account: fromAcc,
+  //     balance: fromAcc.balance,
+  //     date: new Date(ctx.block.timestamp),
+  //   })
+  // );
 
-  await ctx.store.save(
-    new HistoricalBalance({
-      id: `${ctx.event.id}-from`,
-      account: toAcc,
-      balance: toAcc.balance,
-      date: new Date(ctx.block.timestamp),
-    })
-  );
+  // await ctx.store.save(
+  //   new HistoricalBalance({
+  //     id: `${ctx.event.id}-from`,
+  //     account: toAcc,
+  //     balance: toAcc.balance,
+  //     date: new Date(ctx.block.timestamp),
+  //   })
+  // );
 });
 
 processor.run();
@@ -62,17 +65,19 @@ interface TransferEvent {
   amount: bigint;
 }
 
-function getTransferEvent(ctx: EventHandlerContext): TransferEvent {
+function getTransferEvent(ctx: EventHandlerContext) {
   const event = new BalancesTransferEvent(ctx);
-  if (event.isV1020) {
-    const [from, to, amount] = event.asV1020;
-    return { from, to, amount };
-  }
-  if (event.isV1050) {
-    const [from, to, amount] = event.asV1050;
-    return { from, to, amount };
-  }
-  return event.asLatest;
+  console.log(event)
+  // if (event.isV1020) {
+  //   const [from, to, amount] = event.asV1020;
+  //   return { from, to, amount };
+  // }
+  // if (event.isV1050) {
+  //   const [from, to, amount] = event.asV1050;
+  //   return { from, to, amount };
+  // }
+  // return event.asLatest;
+  // return event
 }
 
 async function getOrCreate<T extends { id: string }>(
--- src/types/events.ts
+++ src/types/events.ts
@@ -1,6 +1,7 @@
 import assert from 'assert'
 import {EventContext, Result, deprecateLatest} from './support'
-import * as v9130 from './v9130'
+import * as v5 from './v5'
+import * as v8 from './v8'
 
 export class BalancesTransferEvent {
   constructor(private ctx: EventContext) {
@@ -8,57 +9,42 @@ export class BalancesTransferEvent {
   }
 
   /**
-   *  Transfer succeeded (from, to, value, fees).
+   * Transfer succeeded. \[from, to, value\]
    */
-  get isV1020(): boolean {
-    return this.ctx._chain.getEventHash('balances.Transfer') === '154fca303841d334782de2e871e3572f786f81f86e2a6153c2b9e8dc6fc27422'
-  }
-
-  /**
-   *  Transfer succeeded (from, to, value, fees).
-   */
-  get asV1020(): [Uint8Array, Uint8Array, bigint, bigint] {
-    assert(this.isV1020)
-    return this.ctx._chain.decodeEvent(this.ctx.event)
-  }
-
-  /**
-   *  Transfer succeeded (from, to, value).
-   */
-  get isV1050(): boolean {
+  get isV5(): boolean {
     return this.ctx._chain.getEventHash('balances.Transfer') === '9611bd6b933331f197e8fa73bac36184681838292120987fec97092ae037d1c8'
   }
 
   /**
-   *  Transfer succeeded (from, to, value).
+   * Transfer succeeded. \[from, to, value\]
    */
-  get asV1050(): [Uint8Array, Uint8Array, bigint] {
-    assert(this.isV1050)
+  get asV5(): [v5.AccountId32, v5.AccountId32, bigint] {
+    assert(this.isV5)
     return this.ctx._chain.decodeEvent(this.ctx.event)
   }
 
   /**
    * Transfer succeeded.
    */
-  get isV9130(): boolean {
+  get isV8(): boolean {
     return this.ctx._chain.getEventHash('balances.Transfer') === '99bc4786247456e0d4a44373efe405e598bfadfac87a7c41b0a82a91296836c1'
   }
 
   /**
    * Transfer succeeded.
    */
-  get asV9130(): {from: v9130.AccountId32, to: v9130.AccountId32, amount: bigint} {
-    assert(this.isV9130)
+  get asV8(): {from: v8.AccountId32, to: v8.AccountId32, amount: bigint} {
+    assert(this.isV8)
     return this.ctx._chain.decodeEvent(this.ctx.event)
   }
 
   get isLatest(): boolean {
     deprecateLatest()
-    return this.isV9130
+    return this.isV8
   }
 
-  get asLatest(): {from: v9130.AccountId32, to: v9130.AccountId32, amount: bigint} {
+  get asLatest(): {from: v8.AccountId32, to: v8.AccountId32, amount: bigint} {
     deprecateLatest()
-    return this.asV9130
+    return this.asV8
   }
 }
