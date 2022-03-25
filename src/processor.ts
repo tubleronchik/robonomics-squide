0ccb8169bbedfb0fd0ff11469fd8292b86a8d46a
tubleronchik <m.bystramovich@gmail.com>
2022-03-25 20:48:52 +0300

    remove unused functions

--- src/processor.ts
+++ src/processor.ts
@@ -20,66 +20,13 @@ processor.setDataSource({
   chain: "wss://kusama.rpc.robonomics.network",
 });
 
-
-processor.addEventHandler("balances.Transfer", async (ctx) => {
-  const transfer = getTransferEvent(ctx);
-  // const tip = ctx.extrinsic?.tip || 0n;
-  // const from = ss58.codec(32).encode(transfer.from);
-  // const to = ss58.codec(32).encode(transfer.to);
-
-  // const fromAcc = await getOrCreate(ctx.store, Account, from);
-  // fromAcc.balance = fromAcc.balance || 0n;
-  // fromAcc.balance -= transfer.amount;
-  // fromAcc.balance -= tip;
-  // await ctx.store.save(fromAcc);
-
-  // const toAcc = await getOrCreate(ctx.store, Account, to);
-  // toAcc.balance = toAcc.balance || 0n;
-  // toAcc.balance += transfer.amount;
-  // await ctx.store.save(toAcc);
-
-  // await ctx.store.save(
-  //   new HistoricalBalance({
-  //     id: `${ctx.event.id}-to`,
-  //     account: fromAcc,
-  //     balance: fromAcc.balance,
-  //     date: new Date(ctx.block.timestamp),
-  //   })
-  // );
-
-  // await ctx.store.save(
-  //   new HistoricalBalance({
-  //     id: `${ctx.event.id}-from`,
-  //     account: toAcc,
-  //     balance: toAcc.balance,
-  //     date: new Date(ctx.block.timestamp),
-  //   })
-  // );
-});
-
-processor.addEventHandler("datalog.NewRecord", async (ctx) => {
-  const tr = getDatalogRecord(ctx)
-  // console.log(ctx.event.params[2])
-  console.log(hex_to_ascii(ctx.event.params[2]))
-  // console.log(bin2string(ctx.event.params[2]))
-  // console.log(typeof(ctx.event.params[2]))
-
-})
+processor.addEventHandler("datalog.NewRecord", getDatalogRecord)
 
 processor.run();
 
-// function bin2string(array: DatalogParams){
-// 	var result = "";
-// 	for(var i = 0; i < array.value.length; ++i){
-//     // console.log(array.value[i])
-// 		// result+= (String.fromCharCode(array.value[i]));
-// 	}
-// 	return result;
-// }
-
-function hex_to_ascii(datalog_record: DatalogParams)
+function hex_to_ascii(datalogRecord: DatalogParams)
  {
-	const hex  = datalog_record.value.toString()
+	const hex  = datalogRecord.value.toString()
 	let record = ''
 	for (let n = 0; n < hex.length; n += 2) {
 		record = record.concat(String.fromCharCode(parseInt(hex.substr(n, 2), 16)))
@@ -87,10 +34,9 @@ function hex_to_ascii(datalog_record: DatalogParams)
 	return record
  }
 
-function getDatalogRecord(ctx: EventHandlerContext) {
-  const record = new DatalogNewRecordEvent(ctx)
-  // const [block, store] = record
-  // console.log(String(record.asLatest))
+async function getDatalogRecord(ctx: EventHandlerContext) {
+  let record = new DatalogNewRecordEvent(ctx)
+  console.log(hex_to_ascii(ctx.event.params[2]))
 }
 
 interface DatalogParams {
@@ -99,43 +45,23 @@ interface DatalogParams {
   value: any;
 }
 
-interface TransferEvent {
-  from: Uint8Array;
-  to: Uint8Array;
-  amount: bigint;
-}
 
-function getTransferEvent(ctx: EventHandlerContext) {
-  const event = new BalancesTransferEvent(ctx);
-  // console.log(event)
-  // if (event.isV1020) {
-  //   const [from, to, amount] = event.asV1020;
-  //   return { from, to, amount };
-  // }
-  // if (event.isV1050) {
-  //   const [from, to, amount] = event.asV1050;
-  //   return { from, to, amount };
-  // }
-  // return event.asLatest;
-  // return event
-}
+// async function getOrCreate<T extends { id: string }>(
+//   store: Store,
+//   EntityConstructor: EntityConstructor<T>,
+//   id: string
+// ): Promise<T> {
+//   let entity = await store.get<T>(EntityConstructor, {
+//     where: { id },
+//   });
 
-async function getOrCreate<T extends { id: string }>(
-  store: Store,
-  EntityConstructor: EntityConstructor<T>,
-  id: string
-): Promise<T> {
-  let entity = await store.get<T>(EntityConstructor, {
-    where: { id },
-  });
+//   if (entity == null) {
+//     entity = new EntityConstructor();
+//     entity.id = id;
+//   }
 
-  if (entity == null) {
-    entity = new EntityConstructor();
-    entity.id = id;
-  }
-
-  return entity;
-}
+//   return entity;
+// }
 
 type EntityConstructor<T> = {
   new (...args: any[]): T;
